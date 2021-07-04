8bbb30e396f8d3264d1d4e67aeaefc60b14b93c9
vm230980 <mva_nsk@list.ru>
2021-07-04 11:46:12 +0700

    Исправлена задача "Работа с массивами"

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,45 +1,59 @@
-var array1 = [2, 3, 5, 7, -10, 115, 0, -13, 2];
-console.log("Массив 1: " + array1);
+(function () {
+    var array1 = [2, 3, 5, 7, -10, 115, 0, -13, 2];
+    console.log("Массив 1: " + array1);
 
-function sortDescending(array) {
-    return array.sort(function (a, b) {
-        return b - a;
-    });
-}
+    function sortDescending(array) {
+        return array.sort(function (a, b) {
+            return b - a;
+        });
+    }
 
-console.log("Массив 1, упорядоченный по убыванию: " + sortDescending(array1));
+    console.log("Массив 1, упорядоченный по убыванию: " + sortDescending(array1));
 
-function sliceFirstFive(array) {
-    return array.slice(0, 5);
-}
+    function getFirstFiveElements(array) {
+        return array.slice(0, 5);
+    }
 
-console.log("Подмассив 1 из первых пяти элементов массива 1: " + sliceFirstFive(array1));
+    console.log("Подмассив 1 из первых пяти элементов массива 1: " + getFirstFiveElements(array1));
 
-function sliceLastFive(array) {
-    return array.slice(array.length - 5, array.length);
-}
+    function getLastFiveElements(array) {
+        return array.slice(array.length - 5);
+    }
 
-console.log("Подмассив 2 из последних пяти элементов массива 2: " + sliceLastFive(array1));
+    console.log("Подмассив 2 из последних пяти элементов массива 2: " + getLastFiveElements(array1));
 
-function evenNumbersSum(array) {
-    return array.reduce((res, item, index) => {
-        if (item % 2 === 0) res += item;
-        return res;
-    }, 0);
-}
+    function getEvenNumbersSum(array) {
+        return array.reduce(function (res, item) {
+            var sum;
 
-console.log("Сумма элементов массива 1, которые являются четными числами: " + evenNumbersSum(array1));
+            sum = res;
 
-var array2 = [];
+            if (item % 2 === 0) {
+                sum += item;
+            }
 
-for (var i = 1; i < 101; i++) {
-    array2.push(i);
-}
+            return sum;
+        }, 0);
+    }
 
-console.log("Массив 2: " + array2);
+    console.log("Сумма элементов массива 1, которые являются четными числами: " + getEvenNumbersSum(array1));
 
-function getSquaredEvenNumbers(array) {
-    return array.filter(item => item % 2 === 0).map(item => item * item);
-}
+    var array2 = [];
 
-console.log("Массив квадратов четных чисел из массива 2: " + getSquaredEvenNumbers(array2));
+    for (var i = 1; i <= 100; i++) {
+        array2.push(i);
+    }
+
+    console.log("Массив 2: " + array2);
+
+    function getSquaredEvenNumbers(array) {
+        return array.filter(function (item) {
+            return item % 2 === 0;
+        })
+            .map(function (item) {
+                return item * item;
+            });
+    }
+
+    console.log("Массив квадратов четных чисел из массива 2: " + getSquaredEvenNumbers(array2));
+}());
