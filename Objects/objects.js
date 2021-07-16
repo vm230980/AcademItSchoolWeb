535c0ee721f27c453005425e4b40d83df5245d00
vm230980 <mva_nsk@list.ru>
2021-07-16 19:13:35 +0700

    Исправлена задача "Работа с объектами"

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,83 +1,84 @@
 (function () {
-    var countries = [{
-        name: "Latvia",
-        cities: [
-            {name: "Riga", population: "725000"},
-            {name: "Jurmala", population: "50428"},
-            {name: "Daugavpils", population: "80627"},
-            {name: "Ventspils", population: "34377"},
-        ]
-    }, {
-        name: "Kazakhstan",
-        cities: [
-            {name: "Nūr-Sūltan", population: "1002000"},
-            {name: "Almaty", population: "1777000"},
-            {name: "Semey", population: "320397"},
-            {name: "Oskemen", population: "346127"},
-            {name: "Ridder", population: "48008"},
-            {name: "Shymkent", population: "1074167"}
-        ]
-    },
+    var countries = [
         {
+            name: "Latvia",
+            cities: [
+                {name: "Riga", population: 725000},
+                {name: "Jurmala", population: 50428},
+                {name: "Daugavpils", population: 80627},
+                {name: "Ventspils", population: 34377},
+            ]
+        }, {
+            name: "Kazakhstan",
+            cities: [
+                {name: "Nūr-Sūltan", population: 1002000},
+                {name: "Almaty", population: 1777000},
+                {name: "Semey", population: 320397},
+                {name: "Oskemen", population: 346127},
+                {name: "Ridder", population: 48008},
+                {name: "Shymkent", population: 1074167}
+            ]
+        }, {
             name: "Russia",
             cities: [
-                {name: "Moscow", population: "12506468"},
-                {name: "Kaluga", population: "328871"},
-                {name: "Omsk", population: "1139897"},
-                {name: "Saratov", population: "838321"},
-                {name: "Barnaul", population: "631124"},
-                {name: "Sochi", population: "364171"},
-                {name: "Tumen", population: "816800"}
+                {name: "Moscow", population: 12506468},
+                {name: "Kaluga", population: 328871},
+                {name: "Omsk", population: 1139897},
+                {name: "Saratov", population: 838321},
+                {name: "Barnaul", population: 631124},
+                {name: "Sochi", population: 364171},
+                {name: "Tumen", population: 816800}
             ]
-        },
-        {
+        }, {
             name: "Czech Republic",
             cities: [
-                {name: "Prague", population: "1301132"},
-                {name: "Brno", population: "379526"},
-                {name: "Pilsen", population: "175219"},
-                {name: "Karlovy Vary", population: "48319"},
+                {name: "Prague", population: 1301132},
+                {name: "Brno", population: 379526},
+                {name: "Pilsen", population: 175219},
+                {name: "Karlovy Vary", population: 48319},
             ]
-        },
-        {
+        }, {
             name: "United States Of America",
             cities: [
-                {name: "Washington", population: "5400000"},
-                {name: "New York City", population: "8253213"},
-                {name: "Chicago", population: "2710000"},
-                {name: "Denver", population: "2697476"},
-                {name: "Boston", population: "4628910"},
-                {name: "Seattle", population: "744955"},
-                {name: "Detroit", population: "681090"}
+                {name: "Washington", population: 5400000},
+                {name: "New York City", population: 8253213},
+                {name: "Chicago", population: 2710000},
+                {name: "Denver", population: 2697476},
+                {name: "Boston", population: 4628910},
+                {name: "Seattle", population: 744955},
+                {name: "Detroit", population: 681090}
             ]
-        }];
+        }
+    ];
 
     console.log("Страны: \n");
     countries.forEach(function (country) {
         console.log(country.name + "\n");
     });
 
-    function getMaxCitiesNumberCountriesList(countries) {
-        var maxCitiesNumber = countries[0].cities.length;
+    function getMaxCitiesAmountCountriesList(countries) {
+        if (countries.length === 0) {
+            return [];
+        }
 
-        maxCitiesNumber = countries.reduce(function (res, country) {
-            if (country.cities.length >= maxCitiesNumber) {
-                maxCitiesNumber = country.cities.length;
+        var maxCitiesAmount = countries.reduce(function (res, country) {
+            if (country.cities.length > res) {
+                res = country.cities.length;
             }
 
-            return maxCitiesNumber;
-        }, 0);
+            return res;
+        }, countries[0].cities.length);
 
         return countries.filter(function (country) {
-            return country.cities.length === maxCitiesNumber;
+            return country.cities.length === maxCitiesAmount;
         });
     }
 
-    var maxCitiesNumberCountriesList = getMaxCitiesNumberCountriesList(countries);
+    var maxCitiesAmountCountriesList = getMaxCitiesAmountCountriesList(countries);
 
     console.log("Страны с максимальным количеством городов: \n");
 
-    maxCitiesNumberCountriesList.forEach(function (country) {
+    maxCitiesAmountCountriesList.forEach(function (country) {
         console.log(country.name + "\n");
     });
 
@@ -85,10 +86,8 @@
         var countriesAndPopulationsObject = {};
 
         countries.forEach(function (country) {
-            countriesAndPopulationsObject[country.name] = country.cities.map(function (city) {
-                return city.population;
-            }).reduce(function (res, item) {
-                return res + +item;
+            countriesAndPopulationsObject[country.name] = country.cities.reduce(function (res, city) {
+                return res + city.population;
             }, 0);
         });
 
