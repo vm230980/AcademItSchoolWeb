9511ff72586c95196fcbeb68f07b98a088289c64
vm230980 <mva_nsk@list.ru>
2021-07-19 20:49:53 +0700

    Исправлена задача "Перевод температур"

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -3,14 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
         var celsiusInputTextField = document.getElementById("celsiusInputTextField");
         var fahrenheitOutputTextField = document.getElementById("fahrenheitOutputTextField");
         var kelvinOutputTextField = document.getElementById("kelvinOutputTextField");
+
+        if (celsiusInputTextField.value === "") {
+            document.getElementById("error-message").textContent = "<< Введите значение";
+            fahrenheitOutputTextField.value = "";
+            kelvinOutputTextField.value = "";
+            return;
+        }
+
+        document.getElementById("error-message").textContent = "";
+
         var celsiusValue = parseFloat(celsiusInputTextField.value);
 
-        if (Number.isNaN(celsiusValue)) {
+        if (isNaN(celsiusValue)) {
             fahrenheitOutputTextField.value = "";
             kelvinOutputTextField.value = "";
         } else {
-            fahrenheitOutputTextField.value = celsiusValue * 9 / 5 + 32;
-            kelvinOutputTextField.value = celsiusValue + 273;
+            fahrenheitOutputTextField.value = (celsiusValue * 9 / 5 + 32).toFixed(2);
+            kelvinOutputTextField.value = (celsiusValue + 273.15).toFixed(2);
         }
     });
 });
